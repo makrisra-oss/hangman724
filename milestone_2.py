11ce8333afb78c3ca0f5894b41cbf0a238699b31
makrisra-oss <makrisra@gmail.com>
2024-05-08 11:48:43 +0100

    Update milestone_2.py with user input validation

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,15 @@
+import random
 
 word_list = ['apple', 'banana', 'orange', 'mango', 'pineapple']
 
-print(word_list)
+word = random.choice(word_list)
+
+guess = input("Enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
+#print(word_list)
+#print(word)
