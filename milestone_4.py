3df5c5bdc165ee48749c78d887749a33648227d1
makrisra-oss <makrisra@gmail.com>
2024-05-11 12:49:35 +0100

    m4 update

---
+++ milestone_4.py
@@ -0,0 +1,44 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list).upper()
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word.lower():
+            print(f"Good guess! {guess} is in the word.")
+            for i, letter in enumerate(self.word.lower()):
+                if letter == guess:
+                    self.word_guessed[i] = letter.upper()
+            self.num_letters -= 1
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter: ").lower()
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid input. Please enter a single letter.")
+            elif guess in self.list_of_guesses:
+                print(f"You already guessed the letter {guess}. Please try again.")
+            else:
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                break
+
+# Instantiate the Hangman class
+word_list = ['python', 'javascript', 'ruby', 'java', 'csharp']
+game = Hangman(word_list, num_lives=7)
+
+# Call the ask_for_input method to start the game
+game.ask_for_input()
+
+
